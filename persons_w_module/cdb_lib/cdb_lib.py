63dbe72a79cd6df2ddccdc8cb3066b6080a49c89
atikhomirova <an.tikhomirovaa@gmail.com>
2018-10-26 13:17:22 +0300

    added persons_w_module

--- persons_w_module/cdb_lib/cdb_lib.py
+++ persons_w_module/cdb_lib/cdb_lib.py
@@ -1,5 +1,6 @@
-import google.cloud.storage
+from __future__ import absolute_import
 
+import google.cloud.storage
 
 def llist_blobs(bucket_name, prfx):
     storage_client = google.cloud.storage.Client()
